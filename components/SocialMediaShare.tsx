a3c94ddb1eb13e6f0ef023ac0de66d55c3eb2fe4
lan-party <landon.burress@gmail.com>
2025-01-31 14:45:17 -0600

    added domain email to contact link

--- components/SocialMediaShare.tsx
+++ components/SocialMediaShare.tsx
@@ -9,7 +9,7 @@ const SocialMediaShare = () => {
     <>
 
     <div className='fixed bottom-12 mb-5 md:bottom-0 right-5 md:left-4 md:w-6/12'>
-        <Link href="mailto:landon.burress@gmail.com?subject=Webpage Update Tracker Support" className="link link-hover flex-grow">
+        <Link href="mailto:contact@website-tracker.com?subject=Webpage Update Tracker Support" className="link link-hover flex-grow">
             <p className='flex gap-1'>Contact Support
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
