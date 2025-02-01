671660bba0a6b6069e8cb2547b4a3c979fa5463d
lan-party <landon.burress@gmail.com>
2025-02-01 11:30:41 -0600

    prod bug dix

--- app/confirm-unsubscribe/[webpageId]/page.tsx
+++ app/confirm-unsubscribe/[webpageId]/page.tsx
@@ -8,7 +8,6 @@ export default async function Page({ params, }: {
   params: Promise<{ webpageId: string }>
 }) {
 
-    toast.loading("Disabling alert.")
 
     const webpageId = (await params).webpageId;
     const supabase = createClient(process.env.SUPABASE_URL ? process.env.SUPABASE_URL : "", process.env.SUPABASE_KEY ? process.env.SUPABASE_KEY : "");
