182cd7546eaf4c2b2167682b36bc7efdedd9478f
lan-party <landon.burress@gmail.com>
2025-02-01 11:25:33 -0600

    more unsubscribe tweaks

--- app/confirm-unsubscribe/[webpageId]/page.tsx
+++ app/confirm-unsubscribe/[webpageId]/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { redirect } from "next/navigation";
+import toast from "react-hot-toast";
 import Stripe from "stripe";
 
 
@@ -7,20 +8,26 @@ export default async function Page({ params, }: {
   params: Promise<{ webpageId: string }>
 }) {
 
+    toast.loading("Disabling alert.")
+
     const webpageId = (await params).webpageId;
     const supabase = createClient(process.env.SUPABASE_URL ? process.env.SUPABASE_URL : "", process.env.SUPABASE_KEY ? process.env.SUPABASE_KEY : "");
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ? process.env.STRIPE_SECRET_KEY : "");
 
     // Remove entry from webpages table
+    const { data, error } = await supabase.from('webpages').select().eq('id', webpageId);
+    await supabase.from('log').delete().eq('webpage_id', webpageId);
     await supabase.from('webpages').delete().eq('id', webpageId);
 
     // Remove/disable subscription in stripe
-    const { data, error } = await supabase.from('webpages').select().eq('id', webpageId);
     if(!error && !["", null].includes(data[0].stripe_subscription_id)){
-        await stripe.subscriptions.cancel(data[0].stripe_subscription_id);
+
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ? process.env.STRIPE_SECRET_KEY : "");
+      await stripe.subscriptions.cancel(data[0].stripe_subscription_id);
     }
 
-    redirect('/');
+    setTimeout(
+      redirect('/'),
+      1000);
   
 }
   
