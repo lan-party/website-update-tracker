500aff9456f21c5e66d142ca1639b111eb483abc
lan-party <landon.burress@gmail.com>
2025-02-03 19:26:47 -0600

    added second gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <GoogleAnalytics gaId="G-6XCCGW5SP3" />
       <GoogleAnalytics gaId="AW-443214391" />
       </head>
       <body
